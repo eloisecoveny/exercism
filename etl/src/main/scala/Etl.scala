79bbeb18691620538ee536fb252e1c35e961df94
Eloise Coveny <eloisecoveny@gmail.com>
2020-06-14 10:02:10 +0100

    Refactored Etl solution to use flatMaps

--- etl/src/main/scala/Etl.scala
+++ etl/src/main/scala/Etl.scala
@@ -1,10 +1,9 @@
 object Etl {
   def transform(legacyMap: Map[Int,Seq[String]]): Map[String,Int] = {
-    val newMap = legacyMap.flatMap(res => {
-      val score = res._1
-      val letters = res._2
-      letters.flatMap(letter => Map(letter.toLowerCase() -> score))
-    })
+    val newMap = legacyMap.flatMap {
+        case (score, letters) => letters
+          .flatMap(letter => Map(letter.toLowerCase() -> score))
+    }
     newMap
   }
 }
