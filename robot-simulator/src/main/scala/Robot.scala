c98c16f96ad5b4d72ead427ce540f072c6480774
eloisecoveny <eloisecoveny@gmail.com>
2020-11-13 16:08:40 +0000

    wip

--- robot-simulator/src/main/scala/Robot.scala
+++ robot-simulator/src/main/scala/Robot.scala
@@ -1,23 +1,49 @@
 import Bearing.{Bearing, East, North, South, West}
 
-case class Robot(b: Bearing, c: (Int,Int)) {
-  var bearing: Bearing = Bearing(b.getValue)
-  var coordinates: (Int, Int) = c
+case class Robot(bearing: Bearing, coordinates: (Int,Int)) {
 
-  def turnRight = bearing.turnRight
+  def turnRight: Robot = {
+    bearing match {
+      case North => Robot(Bearing.East, this.coordinates)
+      case East => Robot(Bearing.South, this.coordinates)
+      case South => Robot(Bearing.West, this.coordinates)
+      case West => Robot(Bearing.North, this.coordinates)
+    }
+  }
+
+  def turnLeft: Robot = {
+    bearing match {
+      case North => Robot(Bearing.West, this.coordinates)
+      case East => Robot(Bearing.North, this.coordinates)
+      case South => Robot(Bearing.East, this.coordinates)
+      case West => Robot(Bearing.South, this.coordinates)
+    }
+  }
+
+  def advance: Robot = {
+    val newCoordinates = bearing match {
+      case North => (this.coordinates._1, this.coordinates._2 + 1)
+      case East => (this.coordinates._1 + 1, this.coordinates._2)
+      case South => (this.coordinates._1, this.coordinates._2 - 1)
+      case West => (this.coordinates._1 - 1, this.coordinates._2)
+    }
+    Robot(this.bearing, newCoordinates)
+  }
+
+  def simulate(input: String): Robot = {
+    val instructions = input.split("")
+    instructions.foreach(instruction => {
+      instruction match {
+        case ("R") => this.turnRight
+        case ("L") => this.turnLeft
+        case ("A") => this.advance
+      }
+    })
+    this
+  }
 }
 
 object Bearing extends Enumeration {
-  type Bearing = String
-  val North = "N"
-  val East = "E"
-  val South = "S"
-  val West = "W"
-}
-
-case class Bearing(bearing: Bearing.Value) {
-  val bearings = List(North, East, South, West)
-
-  def getValue = bearing.outerEnum
-  def turnRight = bearings.map(bearing -> bearing == )
+  type Bearing = Value
+  val North, East, South, West = Value
 }
