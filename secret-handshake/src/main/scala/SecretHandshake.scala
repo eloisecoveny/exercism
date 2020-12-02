4ee51473a0713c7f4424a0cfa58506f625ea7b51
Eloise Coveny <eloisecoveny@gmail.com>
2020-12-02 21:38:10 +0000

    Refactor: pattern matching in createSecretHandshake method

--- secret-handshake/src/main/scala/SecretHandshake.scala
+++ secret-handshake/src/main/scala/SecretHandshake.scala
@@ -1,3 +1,7 @@
+import SecretHandshake.{codes, createSecretHandshake}
+
+import scala.annotation.tailrec
+
 object SecretHandshake {
 
   val codeMapping = Map(
@@ -12,35 +16,29 @@ object SecretHandshake {
 
   val range: List[Int] = List(128, 64, 32, 16, 8, 4, 2, 1)
 
-    def commands(int: Int): List[String] = {
-      val binary: Int = convertToBinary(int)
-      val binaryList: List[String] = binary.toString.split("").toList.reverse
-      val numberOfDigits: Int = binaryList.length
-      if (binary < 10000) {
-        createSecretHandshake(numberOfDigits - 1, binaryList)
-      } else {
-        createSecretHandshake(numberOfDigits - 1, binaryList).reverse.tail
-      }
-    }
-
-  def createSecretHandshake(index: Int,
-                            binaryList: List[String],
-                            secretHandshake: List[String] = List.empty[String]): List[String] = {
-    if (index > 0) {
-      if (binaryList(index).toInt == 1) {
-        createSecretHandshake(index - 1, binaryList, codes(index) +: secretHandshake)
-      } else {
-        createSecretHandshake(index - 1, binaryList, secretHandshake)
-      }
+  def commands(int: Int): List[String] = {
+    val binary: Int = convertToBinary(int)
+    val binaryList: List[String] = binary.toString.split("").toList.reverse
+    if (binary < 10000) {
+      createSecretHandshake(binaryList.length - 1, binaryList)
     } else {
-      if (binaryList(index).toInt == 1) {
-        codes(index) +: secretHandshake
-      } else {
-        secretHandshake
-      }
+      createSecretHandshake(binaryList.length - 1, binaryList).reverse.tail
     }
   }
 
+  def createSecretHandshake(index: Int,
+                            binaryList: List[String],
+                            gestures: List[String] = List.empty[String]): List[String] = {
+
+    binaryList(index) match {
+      case "1" if index > 0 => createSecretHandshake(index - 1, binaryList, codes(index) +: gestures)
+      case "1" => codes(index) +: gestures
+      case "0" if index > 0 => createSecretHandshake(index - 1, binaryList, gestures)
+      case "0" => gestures
+    }
+  }
+
+  @tailrec
   def convertToBinary(int: Int, binary: List[String] = List.empty[String], index: Int = 0): Int = {
     if (index < range.length) {
       if (int >= range(index)) {
