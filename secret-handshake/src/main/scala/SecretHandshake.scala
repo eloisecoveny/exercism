e51bec697f2706dabc98e105b3f8cbb042ff8597
Eloise Coveny <eloisecoveny@gmail.com>
2020-12-02 21:55:10 +0000

    Refactor: renaming values for readability

--- secret-handshake/src/main/scala/SecretHandshake.scala
+++ secret-handshake/src/main/scala/SecretHandshake.scala
@@ -1,5 +1,3 @@
-import SecretHandshake.{codes, createSecretHandshake}
-
 import scala.annotation.tailrec
 
 object SecretHandshake {
@@ -12,9 +10,9 @@ object SecretHandshake {
     10000 -> ""
   )
 
-  val codes = List("wink", "double blink", "close your eyes", "jump", "")
+  val gestures = List("wink", "double blink", "close your eyes", "jump", "")
 
-  val range: List[Int] = List(128, 64, 32, 16, 8, 4, 2, 1)
+  val decimals: List[Int] = List(128, 64, 32, 16, 8, 4, 2, 1)
 
   def commands(int: Int): List[String] = {
     val binary: Int = convertToBinary(int)
@@ -26,23 +24,24 @@ object SecretHandshake {
     }
   }
 
+  @tailrec
   def createSecretHandshake(index: Int,
                             binaryList: List[String],
-                            gestures: List[String] = List.empty[String]): List[String] = {
+                            handshake: List[String] = List.empty[String]): List[String] = {
 
     binaryList(index) match {
-      case "1" if index > 0 => createSecretHandshake(index - 1, binaryList, codes(index) +: gestures)
-      case "1" => codes(index) +: gestures
-      case "0" if index > 0 => createSecretHandshake(index - 1, binaryList, gestures)
-      case "0" => gestures
+      case "1" if index > 0 => createSecretHandshake(index - 1, binaryList, gestures(index) +: handshake)
+      case "1" => gestures(index) +: handshake
+      case "0" if index > 0 => createSecretHandshake(index - 1, binaryList, handshake)
+      case "0" => handshake
     }
   }
 
   @tailrec
   def convertToBinary(int: Int, binary: List[String] = List.empty[String], index: Int = 0): Int = {
-    if (index < range.length) {
-      if (int >= range(index)) {
-        convertToBinary(int - range(index), binary :+ "1", index + 1)
+    if (index < decimals.length) {
+      if (int >= decimals(index)) {
+        convertToBinary(int - decimals(index), binary :+ "1", index + 1)
       } else {
         convertToBinary(int, binary :+ "0", index + 1)
       }
