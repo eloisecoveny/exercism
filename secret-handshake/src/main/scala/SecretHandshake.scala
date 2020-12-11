fba733a6876f93b3096741e2bde1fd110074213b
eloisecoveny <eloisecoveny@gmail.com>
2020-12-11 14:49:35 +0000

    Refactoring: Using bitshift instead of index to access the right-most bit
    Co-authored-by: Natasha Thrale <natasha.thrale@bbc.co.uk>

--- secret-handshake/src/main/scala/SecretHandshake.scala
+++ secret-handshake/src/main/scala/SecretHandshake.scala
@@ -2,36 +2,23 @@ import scala.annotation.tailrec
 
 object SecretHandshake {
 
-  val codeMapping = Map(
-    1 -> "wink",
-    10 -> "double blink",
-    100 -> "close your eyes",
-    1000 -> "jump",
-    10000 -> ""
-  )
+  val gestures = List("wink", "double blink", "close your eyes", "jump")
 
-  val gestures = List("wink", "double blink", "close your eyes", "jump", "")
-
-  def commands(int: Int): List[String] = {
-    val binaryString: String = int.toBinaryString
-    val binaryList: List[String] = binaryString.split("").toList.reverse
-    if (binaryString.toInt < 10000) {
-      createSecretHandshake(binaryList.length - 1, binaryList)
-    } else {
-      createSecretHandshake(binaryList.length - 1, binaryList).reverse.tail
-    }
+  def commands(int: Int, gestures: List[String] = gestures): List[String] = {
+    if (int < 16) createSecretHandshake(int, gestures)
+    else createSecretHandshake(int, gestures).reverse
   }
 
   @tailrec
-  def createSecretHandshake(index: Int,
-                            binaryList: List[String],
+  def createSecretHandshake(value: Int,
+                            gestures: List[String],
                             handshake: List[String] = List.empty[String]): List[String] = {
-
-    binaryList(index) match {
-      case "1" if index > 0 => createSecretHandshake(index - 1, binaryList, gestures(index) +: handshake)
-      case "1" => gestures(index) +: handshake
-      case "0" if index > 0 => createSecretHandshake(index - 1, binaryList, handshake)
-      case "0" => handshake
+    if (gestures.isEmpty) handshake
+    else {
+      value.toBinaryString.last match {
+        case '1' => createSecretHandshake (value >> 1, gestures.tail, handshake :+ gestures.head)
+        case _ => createSecretHandshake (value >> 1, gestures.tail, handshake)
+      }
     }
   }
 }
