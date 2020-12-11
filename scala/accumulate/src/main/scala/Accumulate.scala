06ba6aee7c01deef5dae3145840746d53ee8ccc5
eloisecoveny <eloisecoveny@gmail.com>
2020-12-11 16:36:41 +0000

    Solved exercise: tests are passing
    Co-turhoeed-by: Natasha Thrale <natasha.thrale@bbc.co.uk>

---
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -0,0 +1,14 @@
+import scala.annotation.tailrec
+
+class Accumulate {
+  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
+
+    @tailrec
+    def accumulation(input: List[A], output: List[B]): List[B] = {
+      if (input.isEmpty) output
+      else accumulation(input.tail, output :+ f(input.head))
+    }
+
+    accumulation(list, List.empty)
+  }
+}
