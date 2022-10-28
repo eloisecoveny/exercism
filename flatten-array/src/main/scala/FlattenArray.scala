6c5d1877ff3e5d0ab4678875b32396ef4b4fb8d7
Eloise Coveny <eloisecoveny@gmail.com>
2022-10-28 08:30:41 +0100

    wip

--- flatten-array/src/main/scala/FlattenArray.scala
+++ flatten-array/src/main/scala/FlattenArray.scala
@@ -1,14 +1,50 @@
 object FlattenArray {
 
   def flatten(list: List[Any]): List[Any] = {
-    val flattenedList = list
-    flattenedList match {
-      case x :: xs =>
+    println(s"this is the list: $list")
+    list match {
+      case x :: xs => {
+        println(s"ok we've got a list now, head: $x tail: $xs")
         x match {
-          case y :: ys => (y :: flatten(ys)) ::: flatten(xs)
-          case _ => x :: flatten(xs)
+          case y :: ys if y.isInstanceOf[Int] && ys.nonEmpty => {
+            println(s"this should be an int: $y")
+            if (xs.nonEmpty) (y :: flatten(ys)) ::: flatten(xs)
+            else y :: flatten(ys)
+          }
+          case y :: ys if y.isInstanceOf[Int] => {
+            println(s"this should be an int: $y, with empty list $ys")
+            if (xs.nonEmpty) {
+              println(s"... and remaining list $xs")
+              List(y) ::: flatten(xs)
+            }
+            else List(y)
+          }
+          case y :: ys if ys.nonEmpty => {
+            println(s"this should be a list: $y")
+            if (xs.nonEmpty) {
+              println(s"flattening $y and concatening to $ys and then $xs")
+              (flatten(y.asInstanceOf[List[Any]]) :: flatten(ys)) ::: flatten(xs)
+            }
+            else flatten(y.asInstanceOf[List[Any]]) :: flatten(ys)
+          }
+          case y :: ys => {
+            println(s"this should be a list: $y and an empty list $ys")
+            if (xs.nonEmpty) flatten(y.asInstanceOf[List[Any]]) ::: flatten(xs)
+            else flatten(y.asInstanceOf[List[Any]])
+          }
+          case int: Int => {
+            println(s"straight up, got an int: $int")
+            if (xs.nonEmpty) {
+              println(s"...now will concat it with $xs")
+              int :: flatten(xs)
+            }
+            else {
+              println(s"...and will now return ye olde $int")
+              List(int)
+            }
+          }
         }
-      case x => x
+      }
     }
   }
 }
